538ae39e8cbf944bb7764440e8282b50f448492e
serw2203 <serw2203@yandex.ru>
2019-12-15 16:43:47 +0300

    #NONE - init

---
+++ src/main/java/ru/desinger/learnopenshift/LearnOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package ru.desinger.learnopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOpenshiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/desinger/learnopenshift/LearnOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.desinger.learnopenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
