55f5f17861adfe40c35f780b5c897323644f54f5
serw2203 <serw2203@yandex.ru>
2019-12-20 07:40:34 +0300

    #NONE - simple controller

--- src/main/java/ru/desinger/learnopenshift/LearnOpenshiftApplication.java
+++ src/main/java/ru/desinger/learnopenshift/LearnOpenshiftApplication.java
@@ -2,9 +2,19 @@ package ru.desinger.learnopenshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/main")
 public class LearnOpenshiftApplication {
+
+	@GetMapping("/hello")
+	public String route () {
+		return "Hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnOpenshiftApplication.class, args);
